d9a36a61c7fa9908dbee120a6b579fc534b82ad7
jchsaui <gs1gemsthe1@hotmail.com>
2024-05-07 05:02:27 +0700

    demo project commit

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
-function countWords(str) {
-  return str.split(" ").filter((word) => word !== "").length;
+function lowestCommonAncestor(root, p, q) {
+  if (!root || root === p || root === q) return root;
+  const left = lowestCommonAncestor(root.left, p, q);
+  const right = lowestCommonAncestor(root.right, p, q);
+  if (left && right) return root;
+  return left ? left : right;
 }
